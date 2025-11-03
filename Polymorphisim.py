2e6f7f0d05c5a4d9dee86464e652e94e23561554
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2025-11-03 12:19:50 +0530

    questions on Topic polymorphism and magicMethods

---
+++ Polymorphisim.py
@@ -0,0 +1,138 @@
+# duck typing
+# method overriding
+# method overloding
+# operator overloding
+
+
+
+# class Human:
+#     def speak(self):
+#         print("human can speak")
+
+# class Robot:
+#     def speak(self):
+#         print("Dog can speak")
+
+# class Dog:
+#     def speak(self):
+#         print("dog can bark")
+
+# def call_to_speak(entity):
+#     entity.speak()
+
+# for creation in [Human(), Robot(), Dog()]:
+#     call_to_speak(creation)
+
+# print(Human().speak())
+
+
+
+
+# class Animal:
+#     def sound(self):
+#         print("Geniric sound")
+
+# class Dog(Animal):
+#     def sound(slef):
+#         print("Bark")
+
+# class Cat(Animal):
+#     def sound(self):
+#         print("Meow")
+        
+# animal = [Dog(), Cat(), Animal()]
+
+# for a in animal:
+#     a.sound()
+
+
+
+
+# class Math:
+#     def add(self, a, b):
+#         print(a+b)
+#     def add(self, a,b,c):
+#         print(a+b+c)
+# obj = Math()
+# obj.add(1,2,3)
+
+
+
+
+# class Math:
+#     def add(self,*args):
+#         total = sum(args)
+#         print(total)
+
+# obj = Math()
+# obj.add(1,2,3,4,5,4,3,2,1)
+
+
+# print(5+6)
+# print("5"+"6")
+# print([1,2]+[3,4])
+
+
+## duck method
+
+# class Printer:
+#     def work(self):
+#         print("Printer is working")
+
+# class Scanner:
+#     def work(self):
+#         print("Scanner is working")
+
+# for device in [Printer(), Scanner()]:    ##1 way to do it
+#     device.work()
+
+# operate = [Printer(), Scanner()]       ##2nd way ti do it
+
+# for a in operate:
+#     a.work()
+
+
+## over riding method
+
+# class Shape:
+#     def area(self):
+#         print("area")
+
+# class Circle(Shape):
+#     def area(self):
+#         print("PI * r **2")
+
+# class Square(Shape):
+#     def area(self):
+#         print("side square")
+
+# for shape in [Circle(),Square(),Shape()]:
+#     shape.area()
+
+
+
+# class Employe:
+#     def info(self, *args):
+#         print(args)
+
+# details = Employe()
+# details.info("Name: Aryan","Department: IT","Salary: Bhot Jada")
+
+
+# class Employee:
+#     def info(self, *args):
+#         if len(args) == 1:
+#             print(f"Name: {args[0]}")
+#         elif len(args) == 2:
+#             print(f"Name: {args[0]}, Department: {args[1]}")
+#         elif len(args) == 3:
+#             print(f"Name: {args[0]}, Department: {args[1]}, Salary: {args[2]}")
+#         else:
+#             print("Invalid Number of details provided")
+
+# obj = Employee()
+# obj.info("Aryan")
+# obj.info("Aryan","Engineering")
+# obj.info("Aryan", "Engineering",28934571)
+
+
---
+++ magicMethod.py
@@ -0,0 +1,36 @@
+# class Student:
+#     def __init__(self,marks):
+#         self.marks = marks
+    
+#     def __add__(self,other):
+#         return self.marks + other.marks
+    
+#     def __gt__(self,other):
+#         return self.marks > other.marks
+    
+# s1 = Student(90)
+# s2 = Student(89)
+# print(s1 + s2)
+# print(s1 > s2)
+
+
+
+class Distance:
+    def __init__(self,kilometers,meters):
+        self.kilometers = kilometers
+        self.meters = meters
+
+    
+    def __add__(self,other):
+        total_m = self.meters + other.meters
+        total_km = self.kilometers + other.kilometers + total_m // 1000
+        total_m = total_m % 1000
+        return Distance(total_km, total_m)
+    
+    def __str__(self):
+        return f"{self.kilometers} km {self.meters} m"
+
+d1 = Distance(123,123)
+d2 = Distance(371,238)
+d3 = d1 + d2
+print(d3)
