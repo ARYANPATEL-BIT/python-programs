a574b6fc6e982940218af9c64ee5bded237bf270
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2025-11-10 13:42:00 +0530

    Libraries

---
+++ import.py
@@ -0,0 +1,31 @@
+import math
+# print(math.sqrt(25))
+# print(math.floor(5.4))
+# print(math.ceil(4.3))
+# print(math.factorial(7))
+# print(math.sin(45))
+
+
+import random
+
+# num = random.randint(1,90)
+# print(num)
+
+# names = ["Death", "Human", "Martian", "Alien", "Aryan"]
+# random.shuffle(names)
+# print(names)
+
+# print(random.choice(names))
+
+
+import datetime
+
+# now = datetime.datetime.now()
+# print(now)
+
+# date = datetime.date.today()
+# print(now.month)
+# print(now.month)
+# print(now.day)
+# print(date)
+
