1941eb944f88de1576c2dc2fa3d93c380b0debab
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2025-11-16 19:41:56 +0530

    Selection Sort

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -13,20 +13,20 @@
 
 # print(bubble_sort([6,3,8,2,9,5,8,4,1,8,]))
 
-# def bubble_sort(arr):
-#     n = len(arr)
-#     for i in range(n):
-#         swapped = False
-#         for j in range(0,n-i-1):
-#             if arr[j] > arr[j+1]:
-#                 arr[j] , arr[j+1] = arr[j+1] , arr[j]
-#                 swapped = True
-#         if not swapped:
-#             break
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0,n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j] , arr[j+1] = arr[j+1] , arr[j]
+                swapped = True
+        if not swapped:
+            break
 
-#     return arr
+    return arr
 
-# print(bubble_sort([1,2,7,6,9,4,5,0,7,3,4,6,3]))
+print(bubble_sort([1,2,7,6,9,4,5,0,7,3,4,6,3]))
 
 # Time complaxity = O(n^2)
 # Space = O(1)
---
+++ selection_sort.py
@@ -0,0 +1,26 @@
+# def selection_sort(arr):
+#     n = len(arr)
+#     for i in range(n-1):
+#         min_index = i
+#         for j in range(i+1,n):
+#             if arr[j] < arr[min_index]:
+#                 min_index = j
+#         arr[i] , arr[min_index] = arr[min_index] , arr[i]
+#         # print(f"pass {i+1}:", arr)
+#     return arr
+
+# print(selection_sort([1,6,4,7,3,4,9,6,8,4,6,4,3,7,6,4,8,4,5]))
+
+
+# def selection_sort(arr):
+#     n = len(arr)
+#     for i in range(n-1):
+#         min_index = i
+#         for j in range(i+1, n):
+#             if arr[j] < arr[min_index]:
+#                 min_index = j
+#         arr[i] , arr[min_index] = arr[min_index] , arr[i]
+    
+#     return arr
+
+# print(selection_sort([1,5,7,4,3,8,5,9,6,3,4,7,0,4]))
